15b1e04d068eb87e00e5f8770454c741e902592c
Cort Robinson <cortrobinson99@gmail.com>
2021-07-19 19:45:17 -0500

    fix typing for argument

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -4,11 +4,10 @@ Write a type-annotated function sum_list which
 takes a list input_list of floats as argument
 and returns their sum as a float.
 """
-# define type alias floatList which is a list of floats
-floatList = [float]
+from typing import List
 
 
-def sum_list(input_list: floatList) -> float:
+def sum_list(input_list: List[float]) -> float:
     """
     Sum a list of floats.
     """
