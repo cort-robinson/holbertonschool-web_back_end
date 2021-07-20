8d73ffb0c7cec34f27650e91209773fa93262a50
Cort Robinson <cortrobinson99@gmail.com>
2021-07-19 19:17:18 -0500

    add task 0 file

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+defines type-annotated function 'add' that takes
+a float 'a' and a float 'b' as arguments and
+returns their sum as a float
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    adds the arguments 'a' and 'b'
+    :param a: first operand
+    :param b: second operand
+    :return: their sum
+    """
+    return a + b
