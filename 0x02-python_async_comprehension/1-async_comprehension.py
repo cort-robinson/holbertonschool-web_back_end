a85868c9ab6eb281b3f4b1438f2af1f6441f6153
Cort Robinson <cortrobinson99@gmail.com>
2021-07-21 19:40:49 -0500

    update return type annotation

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -9,11 +9,12 @@ return the 10 random numbers.
 """
 import asyncio
 import random
+from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
 
 # create async_comprehension coroutine
-async def async_comprehension() -> list:
+async def async_comprehension() -> List[float]:
     """
     Collect 10 random numbers using an async comprehensing
     """
