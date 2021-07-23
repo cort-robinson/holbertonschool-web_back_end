a5a6e6dbeca4537389e7ea55274430f67c412e3e
Cort Robinson <cortrobinson99@gmail.com>
2021-07-23 14:32:39 -0500

    update task 0 file

--- 0x03-caching/0-basic_cache.py
+++ 0x03-caching/0-basic_cache.py
@@ -20,13 +20,25 @@ BaseCaching = __import__('base_caching').BaseCaching
 
 
 class BasicCache(BaseCaching):
+    """class BasicCache"""
     def __init__(self):
+        """initializer"""
         BaseCaching.__init__(self)
 
     def put(self, key, item):
+        """
+        * Must assign to the dictionary self.cache_data the item value for the
+            key key.
+        * If key or item is None, this method should not do anything.
+        """
         self.cache_data[key] = item
 
     def get(self, key):
+        """
+        * Must return the value in self.cache_data linked to key.
+        * If key is None or if the key doesn’t exist in self.cache_data,
+            return None.
+        """
         if key in self.cache_data:
             return self.cache_data[key]
         return None
