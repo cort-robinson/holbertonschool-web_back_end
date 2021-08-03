41ce53391d03aad1bcb91eee7729aeae655fdbd1
Cort Robinson <cortrobinson99@gmail.com>
2021-08-03 00:28:02 -0500

    update docs

--- 0x05-personal_data/encrypt_password.py
+++ 0x05-personal_data/encrypt_password.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
-"""
-Implement a hash_password function that expects one string argument name
-password and returns a salted, hashed password, which is a byte string.
-
-Use the bcrypt package to perform the hashing (with hashpw).
-"""
+"""contains hash_password and is_valid"""
 import bcrypt
 
 
 def hash_password(password: str) -> bytes:
+    """
+    Implement a hash_password function that expects one string argument name
+    password and returns a salted, hashed password, which is a byte string.
+
+    Use the bcrypt package to perform the hashing (with hashpw).
+    """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
