7323e37cd9becbb0d95dcb751d42839ae1040025
Cort Robinson <cortrobinson99@gmail.com>
2021-08-03 00:24:03 -0500

    add encrypt_password.py for task 5

--- 0x05-personal_data/encrypt_password.py
+++ 0x05-personal_data/encrypt_password.py
@@ -1,1 +1,12 @@
 #!/usr/bin/env python3
+"""
+Implement a hash_password function that expects one string argument name
+password and returns a salted, hashed password, which is a byte string.
+
+Use the bcrypt package to perform the hashing (with hashpw).
+"""
+from bcrypt import hashpw, gensalt
+
+
+def hash_password(password):
+    return hashpw(password.encode('utf-8'), gensalt())
