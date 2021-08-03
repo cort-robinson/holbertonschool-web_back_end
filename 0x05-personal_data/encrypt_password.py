801302f177b8afab872202dbd1335ff00b75f345
Cort Robinson <cortrobinson99@gmail.com>
2021-08-03 00:25:34 -0500

    update for task 5

--- 0x05-personal_data/encrypt_password.py
+++ 0x05-personal_data/encrypt_password.py
@@ -5,8 +5,8 @@ password and returns a salted, hashed password, which is a byte string.
 
 Use the bcrypt package to perform the hashing (with hashpw).
 """
-from bcrypt import hashpw, gensalt
+import bcrypt
 
 
 def hash_password(password):
-    return hashpw(password.encode('utf-8'), gensalt())
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
