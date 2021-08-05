7845e2f7e402d9e0e4b1c16fb68ff43da1f20779
Cort Robinson <cortrobinson99@gmail.com>
2021-08-05 00:37:14 -0500

    add task 9 update to basic_auth.py

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,7 @@
 """Basic authentication module for API"""
 from api.v1.auth.auth import Auth
 import base64
+from typing import Tuple
 
 
 class BasicAuth(Auth):
@@ -32,3 +33,18 @@ class BasicAuth(Auth):
         except Exception:
             return None
         return base64.b64decode(base64_authorization_header).decode('utf-8')
+
+    def extract_user_credentials(
+            self, decoded_base64_authorization_header: str) -> Tuple(str, str):
+        """ Extracts the user credentials from the decoded base64
+            authorization header
+        """
+        if decoded_base64_authorization_header is None:
+            return None, None
+        if not isinstance(decoded_base64_authorization_header, str):
+            return None, None
+        if decoded_base64_authorization_header.count(':') != 1:
+            return None, None
+        else:
+            user_credentials = decoded_base64_authorization_header.split(':')
+            return user_credentials[0], user_credentials[1]
