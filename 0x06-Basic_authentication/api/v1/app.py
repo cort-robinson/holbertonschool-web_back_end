ded5f99c2e88335695732c3c036ade3bea96ab99
Cort Robinson <cortrobinson99@gmail.com>
2021-08-05 00:06:09 -0500

    update for task 5

--- 0x06-Basic_authentication/api/v1/app.py
+++ 0x06-Basic_authentication/api/v1/app.py
@@ -23,16 +23,13 @@ if getenv('AUTH_TYPE') == 'auth':
 def before_request_func():
     """ This function is run before every request
     """
-    if auth:
-        return
-    if not auth.require_auth(request.path, ['/api/v1/status/',
-                                            '/api/v1/unauthorized/',
-                                            '/api/v1/forbidden/']):
-        return
-    if auth.authorization_header(request) is None:
-        abort(401)
-    if auth.current_user(request) is None:
-        abort(403)
+    if auth and auth.require_auth(request.path, ['/api/v1/status/',
+                                                 '/api/v1/unauthorized/',
+                                                 '/api/v1/forbidden/']):
+        if auth.authorization_header(request) is None:
+            abort(401)
+        if auth.current_user(request) is None:
+            abort(403)
 
 
 @app.errorhandler(404)
