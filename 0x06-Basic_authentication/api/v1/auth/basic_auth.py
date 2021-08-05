0b9732edae55c1b15d8f2a371f86c42a804c8e41
Cort Robinson <cortrobinson99@gmail.com>
2021-08-05 01:02:39 -0500

    update for task 10

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -59,6 +59,8 @@ class BasicAuth(Auth):
             return None
         if user_pwd is None or not isinstance(user_pwd, str):
             return None
+        if not DATA.get('User'):
+            return None
         if not User.search({'email': user_email}):
             return None
         else:  # User exists
