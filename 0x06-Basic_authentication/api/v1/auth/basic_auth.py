c09318bcc2cc554f19ac7b1f9d02c85b227385d8
Cort Robinson <cortrobinson99@gmail.com>
2021-08-05 00:22:15 -0500

    add task 8 update to basic_auth.py

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 """Basic authentication module for API"""
+from os import error
 from api.v1.auth.auth import Auth
 
 
 class BasicAuth(Auth):
     """ Basic authentication class
     """
+
     def extract_base64_authorization_header(
             self, authorization_header: str) -> str:
         """Extracts the base64 encoded authorization header"""
@@ -17,3 +19,16 @@ class BasicAuth(Auth):
             return None
         else:
             return authorization_header.replace('Basic ', '', 1)
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """Decodes the base64 encoded authorization header"""
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            base64_authorization_header.decode('base64')
+        except error:
+            return None
+        return base64_authorization_header.decode('utf-8')
