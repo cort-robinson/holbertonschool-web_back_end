8a56cc333a5b3eca8392795c9e6f06877df9bc6b
Cort Robinson <cortrobinson99@gmail.com>
2021-08-11 00:06:46 -0500

    cleanup docstrings

--- 0x07-Session_authentication/api/v1/auth/session_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -9,25 +9,11 @@ from api.v1.auth.auth import Auth
 
 class SessionAuth(Auth):
     """ Session authentication class
-        * validate if everything inherits correctly without overloading
-        * valide the switch by using environment variables
     """
     user_id_by_session_id = {}
 
     def create_session(self, user_id: str = None) -> str:
-        """
-        Create a a session id for a user_id:
-            * Return None if user_id is None
-            * Return None if user_id is not a string
-            * Otherwise:
-                * Generate a Session ID using uuid module and uuid4() like id
-                  in Base
-                * Use this Session ID as key of the dictionary
-                  user_id_by_session_id - the value for this key must be
-                  user_id
-                * Return the Session ID
-            * The same user_id can have multiple sesssion id - indeed, the
-              user_id is the value in the dictionary user_id_by_session_id
+        """ Create a a session id for a user_id:
         """
         if user_id is None or not isinstance(user_id, str):
             return None
