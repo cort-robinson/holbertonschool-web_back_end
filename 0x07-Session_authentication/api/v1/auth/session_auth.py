f89276c532c6a48d6be6cea11ecd318ac6e445f8
Cort Robinson <cortrobinson99@gmail.com>
2021-08-11 12:23:24 -0500

    remove redundant if statements

--- 0x07-Session_authentication/api/v1/auth/session_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -32,10 +32,6 @@ class SessionAuth(Auth):
     def current_user(self, request=None) -> User:
         """ Returns a User instance based on a cookie value:
         """
-        if request is None:
-            return None
         session_id = self.session_cookie(request)
         user_id = self.user_id_for_session_id(session_id)
-        if user_id is None:
-            return None
         return User.get(user_id)
