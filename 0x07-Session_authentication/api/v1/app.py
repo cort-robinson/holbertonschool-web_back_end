23b7a1231e5c35b078718c2b1ab468beb56a86a7
Cort Robinson <cortrobinson99@gmail.com>
2021-08-10 23:17:42 -0500

    add task 0 updates

--- 0x07-Session_authentication/api/v1/app.py
+++ 0x07-Session_authentication/api/v1/app.py
@@ -2,12 +2,12 @@
 """
 Route module for the API
 """
-from os import getenv
-from api.v1.views import app_views
-from flask import Flask, jsonify, abort, request
-from flask_cors import (CORS, cross_origin)
 import os
+from os import getenv
 
+from api.v1.views import app_views
+from flask import Flask, abort, jsonify, request
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -26,6 +26,7 @@ if getenv('AUTH_TYPE') == 'basic_auth':
 def before_request_func():
     """ This function is run before every request
     """
+    request.current_user = auth.current_user(request)
     if auth and auth.require_auth(request.path, ['/api/v1/status/',
                                                  '/api/v1/unauthorized/',
                                                  '/api/v1/forbidden/']):
