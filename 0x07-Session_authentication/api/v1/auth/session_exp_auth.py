343cbf64584c006eae58527dc7c1c5f8f884b6bb
Cort Robinson <cortrobinson99@gmail.com>
2021-08-11 15:28:30 -0500

    update user_id_for_session_id method

--- 0x07-Session_authentication/api/v1/auth/session_exp_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_exp_auth.py
@@ -40,10 +40,10 @@ class SessionExpAuth(SessionAuth):
         if session_id not in self.user_id_by_session_id:
             return None
         if self.session_duration <= 0:
-            return self.user_id_by_session_id[session_id]
+            return self.user_id_by_session_id.get(session_id)
         if 'created_at' not in self.user_id_by_session_id:
             return None
         if (self.user_id_by_session_id.get('created_at') +
                 self.session_duration) < datetime.now():
             return None
-        return self.user_id_by_session_id[session_id]['user_id']
+        return self.user_id_by_session_id.get(session_id).get('user_id')
