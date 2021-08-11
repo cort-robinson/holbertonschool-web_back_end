e09a2d739de46165407799abb3e2697738e1ce08
Cort Robinson <cortrobinson99@gmail.com>
2021-08-11 16:49:26 -0500

    fix import

--- 0x07-Session_authentication/api/v1/app.py
+++ 0x07-Session_authentication/api/v1/app.py
@@ -27,7 +27,7 @@ elif getenv('AUTH_TYPE') == 'session_exp_auth':
     from api.v1.auth.session_exp_auth import SessionExpAuth
     auth = SessionExpAuth()
 elif getenv('AUTH_TYPE') == 'session_db_auth':
-    from api.v1.auth.session_db_auth import SessionDbAuth
+    from api.v1.auth.session_db_auth import SessionDBAuth
 
 
 @app.before_request
