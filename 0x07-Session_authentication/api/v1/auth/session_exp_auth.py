6b79cf6fb15ca86cda03e0e49b97c97017f79188
Cort Robinson <cortrobinson99@gmail.com>
2021-08-11 15:12:14 -0500

    add session_exp_auth

---
+++ 0x07-Session_authentication/api/v1/auth/session_exp_auth.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+"""
+Session expiration authentication module
+"""
+import datetime
+
+from api.v1.auth.session_auth import SessionAuth
+
+
+class SessionExpAuth(SessionAuth):
+    """ Session expiration authentication class
+    """
+    def __init__(self):
+        """ Initialize SessionExpAuth
+        """
+        super().__init__()
+        try:
+            self.session_duration = int(self.env_session_duration)
+        except Exception:
+            self.session_duration = 0
+
+    def create_session(self, user_id=None):
+        """ Create a Session ID
+        """
+        session_id = super().create_session(user_id)
+        if session_id:
+            self.user_id_by_session_id[session_id] = {
+                "user_id": user_id,
+                "created_at": datetime.now()
+            }
+            return session_id
+
+    def user_id_for_session_id(self, session_id=None):
+        """ Return User ID for Session ID
+        """
+        if session_id is None:
+            return None
+        if session_id not in self.user_id_by_session_id:
+            return None
+        if self.session_duration <= 0:
+            return self.user_id_by_session_id[session_id]
+        if 'created_at' not in self.user_id_by_session_id:
+            return None
+        if self.user_id_by_session_id.get('created_at') + \
+                self.session_duration < datetime.now():
+            return None
+        return self.user_id_by_session_id[session_id]
