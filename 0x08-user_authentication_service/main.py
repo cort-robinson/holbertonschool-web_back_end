eee9b6f1406eb576c7a1dd42e82346bf9439ad5f
Cort Robinson <cortrobinson99@gmail.com>
2021-08-18 23:21:51 -0500

    add main.py

---
+++ 0x08-user_authentication_service/main.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+"""
+Main file
+"""
+
+
+def register_user(email: str, password: str) -> None:
+    """
+    Register a new user
+    """
+    pass
+
+
+def log_in_wrong_password(email: str, password: str) -> None:
+    """
+    Log in with wrong password
+    """
+    pass
+
+
+def log_in(email: str, password: str) -> str:
+    """
+    Log in with correct password
+    """
+    pass
+
+
+def profile_unlogged() -> None:
+    """
+    Profile of an unlogged user
+    """
+    pass
+
+
+def profile_logged(session_id: str) -> None:
+    """
+    Profile of a logged user
+    """
+    pass
+
+
+def log_out(session_id: str) -> None:
+    """
+    Log out
+    """
+    pass
+
+
+def reset_password_token(email: str) -> str:
+    """
+    Reset Password Token
+    """
+    pass
+
+
+def update_password(email: str, reset_token: str, new_password: str) -> None:
+    """
+    Update Password
+    """
+    pass
+
+
+EMAIL = "guillaume@holberton.io"
+PASSWD = "b4l0u"
+NEW_PASSWD = "t4rt1fl3tt3"
+
+
+if __name__ == "__main__":
+
+    register_user(EMAIL, PASSWD)
+    log_in_wrong_password(EMAIL, NEW_PASSWD)
+    profile_unlogged()
+    session_id = log_in(EMAIL, PASSWD)
+    profile_logged(session_id)
+    log_out(session_id)
+    reset_token = reset_password_token(EMAIL)
+    update_password(EMAIL, reset_token, NEW_PASSWD)
+    log_in(EMAIL, NEW_PASSWD)
