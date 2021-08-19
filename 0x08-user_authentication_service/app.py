6668c025af77f3381495dcb9ab69430e7a921c8a
Cort Robinson <cortrobinson99@gmail.com>
2021-08-18 22:51:48 -0500

    refactor profile()

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -63,11 +63,10 @@ def profile():
     """Profile route
     """
     session_id = request.cookies.get('session_id')
-    try:
-        user = AUTH.get_user_from_session_id(session_id)
+    user = AUTH.get_user_from_session_id(session_id)
+    if user:
         return jsonify({"email": user.email})
-    except Exception:
-        abort(403)
+    abort(403)
 
 
 if __name__ == '__main__':
