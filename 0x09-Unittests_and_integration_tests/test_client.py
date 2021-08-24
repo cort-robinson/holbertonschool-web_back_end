15dae56bb609f342a1cb3bf7b9940411332eea02
Cort Robinson <cortrobinson99@gmail.com>
2021-08-24 17:48:10 -0500

    update test_client

--- 0x09-Unittests_and_integration_tests/test_client.py
+++ 0x09-Unittests_and_integration_tests/test_client.py
@@ -24,10 +24,23 @@ class TestGithubOrgClient(unittest.TestCase):
         self.assertEqual(client.org(), mock_method())
 
     def test_public_repos_url(self):
-        """Test public_repos_url returns the correct value.
+        """Test _public_repos_url
         """
         with patch('client.GithubOrgClient.org', new_callable=PropertyMock) \
                 as mock_method:
             mock_method.return_value = {'repos_url': 'test_url'}
             client = GithubOrgClient('google')
             self.assertEqual(client._public_repos_url, 'test_url')
+
+    @patch('client.get_json')
+    def test_public_repos(self, mock_get_json):
+        """Test public_repos
+        """
+        mock_get_json.return_value = [{'name': 'test_repo'}]
+        with patch('client.GithubOrgClient._public_repos_url',
+                   new_callable=PropertyMock) as mock_url:
+            client = GithubOrgClient('google')
+            mock_url.return_value = 'test_url'
+            self.assertEqual(client.public_repos(), ['test_repo'])
+            mock_get_json.assert_called_once()
+            mock_url.assert_called_once()
