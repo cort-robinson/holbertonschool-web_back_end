50f7341e2afc9c2945171655a31cad7dfbd98daf
Cort Robinson <cortrobinson99@gmail.com>
2021-08-23 22:38:46 -0500

    update test_utils with new method

--- 0x09-Unittests_and_integration_tests/test_utils.py
+++ 0x09-Unittests_and_integration_tests/test_utils.py
@@ -2,6 +2,7 @@
 """Unittests for utils.py
 """
 import unittest
+
 from parameterized import parameterized
 
 from utils import access_nested_map
@@ -19,3 +20,13 @@ class TestAccessNestedMap(unittest.TestCase):
         """Test access_nested_map
         """
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        [{}, ("a",)],
+        [{"a": 1}, ("a", "b")],
+    ])
+    def test_access_nested_map_exception(self, nested_map, path):
+        """Test access_nested_map
+        """
+        with self.assertRaises(KeyError):
+            access_nested_map(nested_map, path)
