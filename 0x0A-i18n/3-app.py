8b1b2ad25ed11055973bddf640404277e7f59da8
Cort Robinson <cortrobinson99@gmail.com>
2021-08-26 17:19:07 -0500

    update templates

--- 0x0A-i18n/3-app.py
+++ 0x0A-i18n/3-app.py
@@ -26,7 +26,7 @@ def get_locale():
 @app.route('/')
 def index():
     """Index route"""
-    return render_template('1-index.html')
+    return render_template('3-index.html')
 
 
 if __name__ == '__main__':
