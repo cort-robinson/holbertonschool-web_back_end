7bdecad34a4a25808462913f4b1a8a1752d16dfc
Cort Robinson <cortrobinson99@gmail.com>
2021-08-26 18:07:49 -0500

    add task 5 files

--- 0x0A-i18n/5-app.py
+++ 0x0A-i18n/5-app.py
@@ -7,20 +7,19 @@ from typing import Optional
 app = Flask(__name__)
 babel = Babel(app)
 
-
-class Config(object):
-    """Babel config class"""
-    LANGUAGES = ['en', 'fr']
-    BABEL_DEFAULT_LOCALE = 'en'
-    BABEL_DEFAULT_TIMEZONE = 'UTC'
-
-
 users = {
     1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
     2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
     3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
     4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
 }
+
+
+class Config(object):
+    """Babel config class"""
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
 app.config.from_object(Config)
@@ -35,7 +34,7 @@ def get_locale() -> str:
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
-def get_user(login_as: str) -> Optional[dict]:
+def get_user(login_as: int) -> Optional[dict]:
     """Get user"""
     if login_as:
         return users.get(login_as)
@@ -44,15 +43,16 @@ def get_user(login_as: str) -> Optional[dict]:
 @app.before_request
 def before_request():
     """Before requests"""
-    user = get_user(request.args.get('login_as'))
+    user = request.args.get('login_as')
     if user:
-        g.user = user
+        user = int(user)
+    g.user = get_user(user)
 
 
 @app.route('/')
 def index() -> str:
     """Index route"""
-    return render_template('4-index.html')
+    return render_template('5-index.html')
 
 
 if __name__ == '__main__':
