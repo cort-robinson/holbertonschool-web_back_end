52a0626b74a0c8060d62309473ec738912b27c79
Cort Robinson <cortrobinson99@gmail.com>
2021-08-26 18:11:54 -0500

    add task 6 files

---
+++ 0x0A-i18n/6-app.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+"""Basic flask app"""
+from flask import Flask, render_template, request, g
+from flask_babel import Babel
+from typing import Optional
+
+app = Flask(__name__)
+babel = Babel(app)
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+class Config(object):
+    """Babel config class"""
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """Get locale"""
+    locale = request.args.get('locale')
+    if locale and locale in app.config['LANGUAGES']:
+        return locale
+    if g.user:
+        user_locale = g.user.get('locale')
+        if user_locale and user_locale in app.config['LANGUAGES']:
+            return user_locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+def get_user(login_as: int) -> Optional[dict]:
+    """Get user"""
+    if login_as:
+        return users.get(login_as)
+
+
+@app.before_request
+def before_request():
+    """Before requests"""
+    user = request.args.get('login_as')
+    if user:
+        user = int(user)
+    g.user = get_user(user)
+
+
+@app.route('/')
+def index() -> str:
+    """Index route"""
+    return render_template('6-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
