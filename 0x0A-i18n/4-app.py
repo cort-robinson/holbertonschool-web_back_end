521c7cb2143cbdcfc86bcd1ed9db2d2eb7e57d66
Cort Robinson <cortrobinson99@gmail.com>
2021-08-26 17:19:58 -0500

    add task 4 files

---
+++ 0x0A-i18n/4-app.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""Basic flask app"""
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config(object):
+    """Babel config class"""
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale():
+    """Get locale"""
+    locale = request.args.get('locale')
+    if locale and locale in app.config['LANGUAGES']:
+        return locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def index():
+    """Index route"""
+    return render_template('4-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
