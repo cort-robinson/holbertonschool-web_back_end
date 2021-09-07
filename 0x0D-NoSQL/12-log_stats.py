149b810c2a797e687645b60625cf3944bca652ec
Cort Robinson <cortrobinson99@gmail.com>
2021-09-07 17:49:52 -0500

    add task 12 file

---
+++ 0x0D-NoSQL/12-log_stats.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Provides stats about Nginx logs stored in MongoDB"""
+from pymongo import MongoClient
+
+if __name__ == "__main__":
+    data = MongoClient().client.logs.nginx
+
+    print(f'{data.count_documents()} logs\nMethods:')
+    print(f'\tmethod GET: {data.count_documents({"method": "GET"})}')
+    print(f'\tmethod POST: {data.count_documents({"method": "POST"})}')
+    print(f'\tmethod PUT: {data.count_documents({"method": "PUT"})}')
+    print(f'\tmethod PATCH: {data.count_documents({"method": "PATCH"})}')
+    print(f'\tmethod DELETE: {data.count_documents({"method": "DELETE"})}')
+    print(f'{data.count_documents({"path": "/status"})}')
