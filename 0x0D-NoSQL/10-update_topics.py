0525ece88cb25c64f7c90736cc8dd51ee8fe396f
Cort Robinson <cortrobinson99@gmail.com>
2021-09-07 17:34:02 -0500

    update task 10 file

--- 0x0D-NoSQL/10-update_topics.py
+++ 0x0D-NoSQL/10-update_topics.py
@@ -6,7 +6,7 @@ def update_topics(mongo_collection, name, topics):
     """Changes all topics of a school
         document based on the name
     """
-    mongo_collection.update_one(
+    mongo_collection.update(
         {'name': name},
         {'$set': {'topics': topics}}
     )
