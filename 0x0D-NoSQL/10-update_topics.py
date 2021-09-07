5dff0be18fae3aad8977e79a6f6ed0a910c748d2
Cort Robinson <cortrobinson99@gmail.com>
2021-09-07 17:35:27 -0500

    update task 10 file

--- 0x0D-NoSQL/10-update_topics.py
+++ 0x0D-NoSQL/10-update_topics.py
@@ -8,5 +8,6 @@ def update_topics(mongo_collection, name, topics):
     """
     mongo_collection.update(
         {'name': name},
-        {'$set': {'topics': topics}}
+        {'$set': {'topics': topics}},
+        multi=True
     )
