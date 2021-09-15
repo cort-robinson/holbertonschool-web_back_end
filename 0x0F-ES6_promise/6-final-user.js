2c3fe6d695f3c0b143ea3e63e85caf6e2278cfe9
Cort Robinson <cortrobinson99@gmail.com>
2021-09-14 22:14:46 -0500

    add task 6 file

---
+++ 0x0F-ES6_promise/6-final-user.js
@@ -0,0 +1,25 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+/**
+ * Calls the signUpUser function and uploadPhoto
+ * function then returns an array with the
+ * following structure:
+ * [
+ *  {
+ *    status: status of the promise,
+ *    value: value of the promise
+ *  },
+ *  ...
+ * ]
+ * @param {string} firstName first name of the user
+ * @param {string} lastName last name of the user
+ * @param {string} fileName file name of the photo
+ * @returns
+ */
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.all([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ]);
+}
