717a21d42d70c1c0ee5d35ecb145b6145304e08d
Cort Robinson <cortrobinson99@gmail.com>
2021-09-14 21:52:40 -0500

    revert previous commit

--- 0x0F-ES6_promise/3-all.js
+++ 0x0F-ES6_promise/3-all.js
@@ -1,13 +1,8 @@
 import { uploadPhoto, createUser } from './utils';
 
-export default async function handleProfileSignup() {
-  try {
-    const values = await Promise.all([
-      uploadPhoto(),
-      createUser(),
-    ]);
-    return console.log(values[0].body, values[1].firstName, values[1].lastName);
-  } catch (error) {
-    return console.log('Signup system offline');
-  }
+export default function handleProfileSignup() {
+  return Promise.all([
+    uploadPhoto(),
+    createUser(),
+  ]).then((values) => console.log(values[0].body, values[1].firstName, values[1].lastName)).catch(() => console.log('Signup system offline'));
 }
