51633769b61f26ed8627416efa9c49253b7226d6
Cort Robinson <cortrobinson99@gmail.com>
2021-09-15 22:36:11 -0500

    add task 4 file

---
+++ 0x10-ES6_classes/4-pricing.js
@@ -0,0 +1,40 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    if (typeof amount === 'number') {
+      this._amount = amount;
+    }
+    if (currency instanceof Currency) {
+      this._currency = currency;
+    }
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set amount(amount) {
+    if (typeof amount === 'number') {
+      this._amount = amount;
+    }
+  }
+
+  set currency(currency) {
+    if (currency instanceof Currency) {
+      this._currency = currency;
+    }
+  }
+
+  displayFullPrice() {
+    return `${this.amount} ${this.currency.name} (${this.currency.code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
