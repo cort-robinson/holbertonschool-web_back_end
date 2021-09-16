625ca19ba6803c81637866270eb4d5e1de739904
Cort Robinson <cortrobinson99@gmail.com>
2021-09-15 23:42:05 -0500

    add task 7 file

---
+++ 0x10-ES6_classes/7-airport.js
@@ -0,0 +1,20 @@
+const util = require('util');
+
+export default class Airport {
+  constructor(name, code) {
+    if (typeof name === 'string') {
+      this._name = name;
+    }
+    if (typeof code === 'string') {
+      this._code = code;
+    }
+  }
+
+  [util.inspect.custom]() {
+    return `${this.constructor.name} [${this._code}] { _name: '${this._name}', _code: '${this._code}' }`;
+  }
+
+  toString() {
+    return `[object ${this._code}]`;
+  }
+}
