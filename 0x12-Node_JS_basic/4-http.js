d719149d1c18bf64f2b7468e6ed5fd4481b27df3
Cort Robinson <cortrobinson99@gmail.com>
2021-09-26 14:16:53 -0500

    add task 4 file

---
+++ 0x12-Node_JS_basic/4-http.js
@@ -0,0 +1,12 @@
+const http = require('http');
+
+// create http server
+const app = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello Holberton School!');
+});
+
+// listen http server
+app.listen(1245, () => {
+  console.log('Hello Holberton School!');
+});
