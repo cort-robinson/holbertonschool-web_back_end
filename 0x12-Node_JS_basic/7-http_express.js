2a73aa678241ffeec9ef7a635b2e5313a0342b99
Cort Robinson <cortrobinson99@gmail.com>
2021-09-26 23:08:38 -0500

    add task 7 file

---
+++ 0x12-Node_JS_basic/7-http_express.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const countStudents = require('./3-read_file_async');
+
+const app = express();
+const filePath = process.argv[2];
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.get('/students', (req, res) => {
+  countStudents(filePath)
+    .then((data) => {
+      res.send(data.join('\n'));
+    });
+});
+
+app.listen(1245, () => {
+  console.log('Listening on port 1245');
+});
+
+module.exports = app;
