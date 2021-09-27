0b478dea2901e122af6727d463bbab3214457e7f
Cort Robinson <cortrobinson99@gmail.com>
2021-09-27 12:20:26 -0500

    update server.js and index.js

--- 0x12-Node_JS_basic/full_server/routes/index.js
+++ 0x12-Node_JS_basic/full_server/routes/index.js
@@ -3,9 +3,6 @@ import StudentsController from '../controllers/StudentsController';
 
 export default (app) => {
   app.get('/', AppController.index);
-};
-
-export const students = (app) => {
   app.get('/students', StudentsController.index);
   app.get('/students/:major', StudentsController.index);
 };
--- 0x12-Node_JS_basic/full_server/server.js
+++ 0x12-Node_JS_basic/full_server/server.js
@@ -1,7 +1,10 @@
 import express from 'express';
+import index from './routes/index';
 
 // create express app
 const app = express();
+
+index(app);
 
 app.listen(1245, () => {
   console.log('server started');
