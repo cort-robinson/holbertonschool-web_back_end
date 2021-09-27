a3baa9a51b4994861299c543ac20b0cecca0929f
Cort Robinson <cortrobinson99@gmail.com>
2021-09-27 11:30:53 -0500

    update StudentsController.js

--- 0x12-Node_JS_basic/full_server/controllers/StudentsController.js
+++ 0x12-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -2,22 +2,30 @@ import readDatabase from '../utils';
 
 export default class StudentsController {
   static getAllStudents(req, res) {
-    res.status(200).write('This is the list of our students');
+    res.write('This is the list of our students');
     readDatabase('database.csv')
       .then((data) => {
-        for (const field of data) {
-          res.write(`Number of students in ${field.name}. List: ${field}`);
+        for (const [field, names] of Object.entries(data)) {
+          res.write(`Number of students in ${field}. List: ${names.join(', ')}`);
         }
+        res.end();
+      })
+      .catch((err) => {
+        res.status(500).write(err);
+        res.end();
       });
   }
 
   static getAllStudentsByMajor(req, res) {
-    res.status(200).write('This is the list of our students');
-    readDatabase('database.csv')
-      .then((data) => {
-        for (const field of data) {
-          res.write(`Number of students in ${field.name}. List: ${field}`);
-        }
-      });
+    if (req.params.major !== 'CS' && req.params.major !== 'SWE') {
+      res.status(500).send('Major parameter must be CS or SWE');
+    } else {
+      const { major } = req.params;
+      readDatabase('database.csv')
+        .then((data) => {
+          res.write(`List: ${data[major].join(', ')}`);
+        });
+      res.end();
+    }
   }
 }
