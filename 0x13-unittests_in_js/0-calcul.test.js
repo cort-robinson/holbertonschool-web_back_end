1ac9bc7da4fa5cb2667a88620a64753b21c7d032
Cort Robinson <cortrobinson99@gmail.com>
2021-09-29 17:14:05 -0500

    update task 0 files

--- 0x13-unittests_in_js/0-calcul.test.js
+++ 0x13-unittests_in_js/0-calcul.test.js
@@ -1,8 +1,17 @@
 const assert = require('assert');
 const calculateNumber = require('./0-calcul');
 
-describe('calculateNumber', function() {
-  it('Returns 4 when adding 1 and 3', function() {
+describe('calculateNumber', function () {
+  it('Returns 4 when adding 1 and 3', function () {
     assert.equal(calculateNumber(1, 3), 4);
+  });
+  it('Returns 5 when adding 1 and 3.7', function () {
+    assert.equal(calculateNumber(1, 3.7), 5);
+  });
+  it('Returns 5 when adding 1.2 and 3.7', function () {
+    assert.equal(calculateNumber(1.2, 3.7), 5);
+  });
+  it('Returns 6 when adding 1.5 and 3.7', function () {
+    assert.equal(calculateNumber(1.5, 3.7), 6);
   });
 });
