b9c687097549ec89674d0e39f4d824f9c8fba0d9
Cort Robinson <cortrobinson99@gmail.com>
2021-10-01 15:28:54 -0500

    update task 1 file

--- 0x14-queuing_system_in_js/0-redis_client.js
+++ 0x14-queuing_system_in_js/0-redis_client.js
@@ -1,10 +1,8 @@
 import { createClient } from 'redis';
 
-(async () => {
-  const client = createClient();
+const client = createClient();
 
-  client.on('error', (err) => console.log(`Redis client not connected to the server: ${err.message}`));
-  client.on('connect', () => console.log('Redis client connected to the server'));
+client.on('error', (err) => console.log(`Redis client not connected to the server: ${err.message}`));
+client.on('connect', () => console.log('Redis client connected to the server'));
 
-  client.connect();
-})();
+client.connect();
