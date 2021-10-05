85020577385157a859982071f0bc775328ab0a8e
Cort Robinson <cortrobinson99@gmail.com>
2021-10-05 14:51:21 -0500

    add task 7 and 8 files

---
+++ 0x14-queuing_system_in_js/7-job_processor.js
@@ -0,0 +1,20 @@
+import kue from 'kue';
+
+const blackList = [
+  '4153518780',
+  '4153518781',
+];
+
+function sendNotification(phoneNumber, message, job, done) {
+  job.progress(0, 100);
+  if (blackList.indexOf(phoneNumber) !== -1) return done(new Error(`Phone number ${phoneNumber} is blacklisted`));
+  job.progress(50, 100);
+  console.log(`Sending notification to ${phoneNumber}, with message: ${message}`);
+  done();
+}
+
+const queue = kue.createQueue();
+
+queue.process('push_notification_code_2', 2, function (job, done) {
+  sendNotification(job.data.phoneNumber, job.data.message, job, done);
+});
